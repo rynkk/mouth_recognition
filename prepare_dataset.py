1f74bc96775edd622aa2c00573f7260f4069e54a
Daniel Kamphake <daniel.kamphake@googlemail.com>
2019-11-28 19:12:55 +0100

    creating dataset dataframe & updating gitignore

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 import os
 from collections import Counter
+import pandas as pd
 
-def prepare_videos(filepath = './dataset/cropped_videos/'):
+
+def prepare_videos(filepath='./dataset/cropped_videos/'):
     videos = []
     for index, file in enumerate(sorted(os.listdir(filepath))):
         if index < 119:
@@ -27,31 +29,34 @@ def prepare_videos(filepath = './dataset/cropped_videos/'):
     print(np.array(videos).shape)
     print(np.array(videos)[0].shape)
 
-def prepare_labels(filepath = './dataset/alignments/'):
-    labels = []
-    i = 0
-    for index, file in enumerate(sorted(os.listdir(filepath))):
-        if i == 0:
-            #i += 1
-            with open(filepath + file, 'r') as f:
-                lines = f.readlines()
-                label = [(int(y[0])/1000, int(y[1])/1000, y[2]) for y in [x.strip().split(" ") for x in lines]]
-            label = strip(label, ['sp','sil'])
-            #print(label)
-            #print(np.array(label))
-            for item in np.array(label).ravel():
 
-                if not str(item).isnumeric():
-                    labels.append(item)
-    print(np.array(labels).shape)
-    cnt = Counter(np.array(labels).ravel())
-    for elem in sorted(cnt):
-        print(elem)
-
-def strip(align, items):
-    return [sub for sub in align if sub[2] not in items]
+def prepare_labels(poss_labels, alignpath='./dataset/alignments/', videopath='./dataset/cropped_videos/'):
+    r = {'videopath': []}
+    for po in poss_labels:
+        r[po] = []
+    video_paths = sorted(os.listdir(videopath))
+    for index, file in enumerate(sorted(os.listdir(alignpath))):
+        with open(alignpath + file, 'r') as f:
+            lines = f.readlines()
+            label = [y[2] for y in [x.strip().split(" ") for x in lines]]
+            label = set(label)
+            label.remove('sil')
+            r['videopath'].append(video_paths[index])
+            for item in poss_labels:
+                if item in label:
+                    r[item].append(1)
+                else:
+                    r[item].append(0)
+    return r
 
 
 if __name__ == '__main__':
-    #prepare_videos()
-    prepare_labels()
+    # prepare_videos()
+    labels = ['again', 'at', 'bin', 'blue', 'by', 'eight', 'five', 'four', 'green', 'in',
+              'lay', 'place', 'please', 'nine', 'now', 'one', 'red', 'set', 'seven', 'six', 'soon',
+              'three', 'two', 'white', 'with', 'zero', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+              'j', 'k', 'l', 'm', 'n', 'o', 'q', 'r', 's', 't', 'p', 'u', 'v', 'x', 'y', 'z']
+    df = pd.DataFrame.from_dict(prepare_labels(labels))
+    print(df)
+
+
