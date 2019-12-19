005d64eb35efa8c5a553e44ece8b311df656d7d2
Daniel Kamphake <daniel.kamphake@googlemail.com>
2019-12-19 03:37:50 +0100

    debug changes

--- data_gen_stanford.py
+++ data_gen_stanford.py
@@ -7,24 +7,27 @@ import os
 import random
 
 lipnet_features = ['again', 'at', 'bin', 'blue', 'by', 'eight', 'five', 'four', 'green', 'in',
-              'lay', 'place', 'please', 'nine', 'now', 'one', 'red', 'set', 'seven', 'six', 'soon',
-              'three', 'two', 'white', 'with', 'zero', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-              'j', 'k', 'l', 'm', 'n', 'o', 'q', 'r', 's', 't', 'p', 'u', 'v', 'x', 'y', 'z']
+                   'lay', 'place', 'please', 'nine', 'now', 'one', 'red', 'set', 'seven', 'six', 'soon',
+                   'three', 'two', 'white', 'with', 'zero', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                   'j', 'k', 'l', 'm', 'n', 'o', 'q', 'r', 's', 't', 'p', 'u', 'v', 'x', 'y', 'z']
+
 
 class DataGenerator(Sequence):
 
-    def __init__(self, batch_size=10, dim=(75,50,100), n_channels=3, n_classes=51, val_split=0.9, shuffle=True):
+    def __init__(self, batch_size=10, dim=(75, 50, 100), n_channels=3, n_classes=51, val_split=0.99, shuffle=True):
         self.dim = dim
         self.batch_size = batch_size
         self.df = self.get_dataframe(lipnet_features)
         test_train_IDs = list(range(len(self.df.videopath)))
         random.shuffle(test_train_IDs)
-        self.train_IDs = test_train_IDs[:int(len(test_train_IDs) * (1-val_split))]
-        self.valid_IDs = test_train_IDs[int(len(test_train_IDs) * val_split):]
+        max_train_index = len(test_train_IDs) - int(len(test_train_IDs) * (1 - val_split))
+        self.train_IDs = test_train_IDs[0:max_train_index]
+        self.valid_IDs = test_train_IDs[max_train_index:]
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.shuffle = shuffle
         self.on_epoch_end()
+        self.test_index = 0
 
     def __len__(self):
         # 'Denotes the number of batches per epoch'
@@ -33,7 +36,7 @@ class DataGenerator(Sequence):
     def __getitem__(self, index):
         # 'Generate one batch of data'
         # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
 
         # Find list of IDs
         train_IDs_temp = [self.train_IDs[k] for k in indexes]
@@ -45,26 +48,22 @@ class DataGenerator(Sequence):
 
     def on_epoch_end(self):
         self.indexes = np.arange(len(self.train_IDs))
-        if self.shuffle == True:
+        if self.shuffle:
             np.random.shuffle(self.indexes)
-
 
     def __data_generation(self, list_IDs_temp):
         # 'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         # Initialization
+        print(list_IDs_temp)
         X = np.empty((self.batch_size, *self.dim, self.n_channels))
         y = np.empty((self.batch_size, self.n_classes), dtype=int)
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
             # Store sample
-
-            y[i]  = np.array(self.df.iloc[ID, 2:].values)
-            X[i,] = np.array(self.load_video(self.df.iloc[ID, 1]))
-
+            y[i] = np.array(self.df.iloc[ID, 2:].values)
+            X[i] = np.array(self.load_video(self.df.iloc[ID, 1]))
         return X, y
-
-
 
     def get_dataframe(self, poss_labels, alignpath='./dataset/alignments/', videopath='./dataset/cropped_videos/'):
         if 'train.csv' in os.listdir('./dataset/'):
@@ -76,7 +75,7 @@ class DataGenerator(Sequence):
         video_paths = sorted(os.listdir(videopath))
         broken_index = 0
         for index, file in enumerate(sorted(os.listdir(alignpath))):
-            print(str(np.round(index/len(os.listdir(alignpath)) *100, decimals=1)), end="\r")
+            print(str(np.round(index / len(os.listdir(alignpath)) * 100, decimals=1)), end="\r")
             with open(alignpath + file, 'r') as f:
                 cap = cv2.VideoCapture(videopath + video_paths[index])
                 frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -98,24 +97,25 @@ class DataGenerator(Sequence):
         r.to_csv("dataset/train.csv", sep='\t')
         return r
 
-
     def load_video(self, filepath):
         cap = cv2.VideoCapture(filepath)
-        frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        
-        buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        for count in range(frameCount):
+        buf = np.empty((frame_count, frame_height, frame_width, 3), np.dtype('uint8'))
+
+        for count in range(frame_count):
             ret, buf[count] = cap.read()
         cap.release()
-        return buf / 255
+        #print("loaded_videos = " + str(self.test_index))
+        self.test_index += 1
+        return buf
 
     def get_valid_data(self):
         X = np.empty((len(self.valid_IDs), *self.dim, self.n_channels))
         y = np.empty((len(self.valid_IDs), self.n_classes), dtype=int)
         for index, ID in enumerate(self.valid_IDs):
-            y[index]  = np.array(self.df.iloc[ID, 2:].values)
+            y[index] = np.array(self.df.iloc[ID, 2:].values)
             X[index,] = np.array(self.load_video(self.df.iloc[ID, 1]))
         return X, y
--- lipnet_NN.py
+++ lipnet_NN.py
@@ -1,61 +1,70 @@
 from keras.layers import Input, ZeroPadding3D, Conv3D, BatchNormalization, Activation, SpatialDropout3D, MaxPooling3D, \
     TimeDistributed, Flatten, Bidirectional, GRU, Dense, AveragePooling3D
 from keras import Model
+import tensorflow as tf
 import keras
 import numpy as np
 from data_gen_stanford import DataGenerator
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(gpus)
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
 
 
 # https://github.com/rizkiarm/LipNet/blob/master/lipnet/model2.py
 
 def get_Lipnet(n_classes=10, summary=False):
     input_layer = Input(name='the_input', shape=(75, 50, 100, 3), dtype='float32')
-    network = Conv3D(32, (3, 5, 5), strides=(1, 2, 2), padding="same", kernel_initializer='he_normal', name='conv1')(input_layer)
+    network = Conv3D(32, (3, 5, 5), strides=(1, 2, 2), padding="same", kernel_initializer='he_normal', name='conv1')(
+        input_layer)
     network = BatchNormalization(name='batc1')(network)
     network = Activation('relu', name='actv1')(network)
     network = SpatialDropout3D(0.5)(network)
     network = MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2), name='max1')(network)
 
-    network = Conv3D(64, (3, 5, 5), strides=(1, 1, 1), padding="same", kernel_initializer='he_normal', name='conv2')(network)
+    network = Conv3D(64, (3, 5, 5), strides=(1, 1, 1), padding="same", kernel_initializer='he_normal', name='conv2')(
+        network)
     network = BatchNormalization(name='batc2')(network)
     network = Activation('relu', name='actv2')(network)
     network = SpatialDropout3D(0.5)(network)
     network = MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2), name='max2')(network)
 
-    network = Conv3D(96, (3, 3, 3), strides=(1, 1, 1), padding="same", kernel_initializer='he_normal', name='conv3')(network)
+    network = Conv3D(96, (3, 3, 3), strides=(1, 1, 1), padding="same", kernel_initializer='he_normal', name='conv3')(
+        network)
     network = BatchNormalization(name='batc3')(network)
     network = Activation('relu', name='actv3')(network)
     network = SpatialDropout3D(0.5)(network)
     network = MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2), name='max3')(network)
 
-    #network = TimeDistributed(Flatten())(network)
+    network = TimeDistributed(Flatten())(network)
 
-    #network = Bidirectional(GRU(128, return_sequences=True, kernel_initializer='Orthogonal', name='gru1'),
-    #                        merge_mode='concat')(network)
-    #network = Bidirectional(GRU(128, return_sequences=True, kernel_initializer='Orthogonal', name='gru2'),
-    #                        merge_mode='concat')(network)
-    network = Conv3D(64, (1, 1, 1))(network)
-    network = AveragePooling3D(pool_size=(2, 2, 2))(network)
+    network = Bidirectional(GRU(128, return_sequences=True, kernel_initializer='Orthogonal', name='gru1'),
+                            merge_mode='concat')(network)
+    network = Bidirectional(GRU(128, return_sequences=True, kernel_initializer='Orthogonal', name='gru2'),
+                            merge_mode='concat')(network)
     network = Flatten()(network)
-    outputs = Dense(n_classes, kernel_initializer='he_normal', name='dense1', activation="sigmoid")(network)
+    outputs = Dense(n_classes, kernel_initializer='he_normal', name='dense1', activation="softmax")(network)
 
     model = Model(inputs=input_layer, outputs=outputs)
     if summary:
         keras.utils.plot_model(model, 'network.png', show_shapes=True)
         print(model.summary())
 
-    model.compile(optimizer=keras.optimizers.adam(lr=1e-4),
-                  loss=keras.losses.binary_crossentropy,
-                  metrics=['accuracy'])
+    model.compile(loss=keras.losses.binary_crossentropy,
+                  optimizer=keras.optimizers.Adam(beta_1=0.9, beta_2=0.999, lr=1e-4),
+                  metrics=['acc', 'mse', keras.metrics.AUC()])
     return model
 
 
 if __name__ == '__main__':
-    model = get_Lipnet(n_classes=51, summary=True)
-    datagen = DataGenerator(batch_size=10, val_split=0.99)
-    if False:
-        model.fit_generator(generator=datagen, epochs=1, shuffle=True, 
-            use_multiprocessing=True, workers=6)
-    else:
-        model.fit_generator(generator=datagen, epochs=1, shuffle=True, validation_data=datagen.get_valid_data())
-        
+    model = get_Lipnet(n_classes=51, summary=False)
+
+    data_gen = DataGenerator(batch_size=10, val_split=0.99)
+    model.fit_generator(generator=data_gen, epochs=1, shuffle=True, validation_data=data_gen.get_valid_data())
+    model.save("model")
+    # print(model.evaluate(datagen, batch_size=10, steps=1))
